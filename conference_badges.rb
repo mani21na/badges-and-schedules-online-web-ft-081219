31da790f7d72742a1289098f3e10796e9ae08473
Hanna Lee <mani21na@gmail.com>
2019-08-14 22:40:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@
 conference_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz "]
 
 def badge_maker (name)
-    puts "Hello, my name is #{name}."
+    return "Hello, my name is #{name}."
 end
 
 
