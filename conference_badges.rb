b69e3ab225d057dba45cc15808d1bd4303dff46a
Hanna Lee <mani21na@gmail.com>
2019-08-14 22:44:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,9 @@ def badge_maker (name)
 end
 
 
-def batch_badge_creator (conference_speakers)
+def batch_badge_creator (array)
   list_badge_message = []
-  conference_speakers.each {|attend| list_badge_message << badge_maker(attend)}
+  array.each {|attend| list_badge_message << badge_maker(attend)}
   list_badge_message
 end
 
