f907bd64bbcf730e941426601140808e7c9f2d19
Hanna Lee <mani21na@gmail.com>
2019-08-14 22:40:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,8 @@
 # Write your code here.
 conference_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz "]
 
-def badge_maker(conference_speakers)
-  conference_speakers.each do |name|
+def badge_maker(name)
     puts "Hello, my name is #{name}."
-  end
 end
 
 
