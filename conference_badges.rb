b025d8462b27f7e613fd9f816f9e46b2331f1335
Hanna Lee <mani21na@gmail.com>
2019-08-14 22:38:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@
 conference_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz "]
 
 def badge_maker(array)
-  array.each {|name| puts "Hello, my name is #{name}."}
+  array.each{|name| puts "Hello, my name is #{name}."}
 end
 
 
